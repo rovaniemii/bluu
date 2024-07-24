4a01c900e6103a8f700f50b7a23b90b77cb4ee3e
송현영 <rovaniemiiiii@gmail.com>
2024-07-24 13:56:05 +0900

    feat: 라이브러리 버전 업, 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,11 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:designsystem"))
     implementation(project(":core:data"))
+    implementation(project(":core:database"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:model"))
+    implementation(project(":core:network"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
